64114ca7bc69940e9e9a408f844c68b0ad1d8111
vincentetaix <vincentetaix@gmail.com>
2019-01-29 11:26:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/td1/Controller.java
@@ -0,0 +1,25 @@
+package com.example.td1;
+
+public class Controller {
+    public Controller(MainActivity activity){
+        this.activity = activity;
+    }
+
+    public void OnCreate(){
+        //listpkm=DL data();
+        //storedata(listpkm);
+        //activity.ShowList(listpkm);
+    }
+
+    private void storedata(){
+        //TODO
+    }
+
+    /*
+    public void ShowList(listpkm){
+        //TODO
+    }
+    */
+
+    private MainActivity activity;
+}
